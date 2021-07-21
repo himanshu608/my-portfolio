d36c3f59302f4605e039b09c077fd560628df801
Himanshu <61231732+himanshu608@users.noreply.github.com>
2021-07-21 19:32:38 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,6 +32,26 @@ $(window).scroll(function(){
 
 
 });
+
+$(document).ready(function() {
+    // get the anchor link buttons
+    const menuBtn = $('.remove-hash');
+    // when each button is clicked
+    menuBtn.click(()=>{	
+      // set a short timeout before taking action
+      // so as to allow hash to be set
+      setTimeout(()=>{
+        // call removeHash function after set timeout
+        removeHash();
+      }, 5); // 5 millisecond timeout in this case
+    });
+  
+    // removeHash function
+    // uses HTML5 history API to manipulate the location bar
+    function removeHash(){
+      history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
+    }
+  });
 function fun(){
     if($(window).width()>930){
         $("#menu-bars").addClass("hide");
